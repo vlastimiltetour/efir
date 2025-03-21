5aaa52449e83e3c2ffb8861bf77bae36a8c37391
vlastimil.tetour <v.tetour@gmail.com>
2025-03-21 09:06:26 +0100

    added pdf order attachment

--- efir/orders/mail_confirmation.py
+++ efir/orders/mail_confirmation.py
@@ -1,4 +1,5 @@
 import logging
+import ssl
 import urllib.error
 from io import BytesIO
 from smtplib import (SMTPDataError, SMTPException, SMTPRecipientsRefused,
@@ -8,6 +9,7 @@ import weasyprint
 from django.core.mail import EmailMultiAlternatives
 from django.http import HttpResponse
 from django.template.loader import render_to_string
+from requests.exceptions import SSLError
 
 from .models import Order
 
@@ -22,102 +24,84 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
-def customer_order_email_confirmation(order_id):
-    order = Order.objects.get(id=order_id)
+def customer_order_email_confirmation(order_id: int) -> bool:
+    try:
+        order = Order.objects.get(id=order_id)
+    except Order.DoesNotExist:
+        logger.error('Order with this ID does not exist', order_id)
+        return False
+    
+    if order.paid_confirmation_sent:
+        logger.info('Order confirmation has been already sent')
+        return False 
 
-    if not order.paid_confirmation_sent:
-            
+    pdf_out = BytesIO()
+    html_content = render_to_string(
+            "orders/customer_email_confirmation.html", {"order": order}
+        )
+    
+    try: 
+        pdf_content = render_to_string("orders/invoice_pdf.html", {"order": order})
+        stylesheet_url = (
+            "https://etb.fra1.cdn.digitaloceanspaces.com/etb/css/styles.css"
+        )
+        # stylesheet_url = f"{settings.STATIC_URL}css/styles.css"
+
         try:
-            logger.info(
-                "Attempting to send customer order confirmation email for order ID %s",
-                order_id,
-            )
+            stylesheets = [weasyprint.CSS(stylesheet_url)]
+        except urllib.error.URLError as e:
+            logger.error("Failed to fetch stylesheet: %s", e)
+            stylesheets = []  # Proceed without stylesheets if fetching fails
 
-            order = Order.objects.get(id=order_id)
-            pdf = render_to_string("orders/invoice_pdf.html", {"order": order})
-            out = BytesIO()
-            stylesheet_url = (
-                "https://etb.fra1.cdn.digitaloceanspaces.com/etb/css/styles.css"
-            )
-            # stylesheet_url = f"{settings.STATIC_URL}css/styles.css"
-            print("this is stylesheet url", stylesheet_url)
+        weasyprint.HTML(string=pdf_content).write_pdf(
+                pdf_out, stylesheets=stylesheets
+            ) 
+      
+    except Exception as e:
+        logger.error('Failed to generate a PDF for order ID %s: %s', order_id, e)
+        pdf_out = None
 
-            try:
-                stylesheets = [weasyprint.CSS(stylesheet_url)]
-                weasyprint.HTML(string=pdf).write_pdf(out, stylesheets=stylesheets)
-            except urllib.error.URLError as e:
-                logger.error("Failed to fetch stylesheet: %s", e)
-                stylesheets = []  # Proceed without stylesheets if fetching fails
 
-            html_content = render_to_string(
-                "orders/customer_email_confirmation.html", {"order": order}
-            )
-            msg = EmailMultiAlternatives(
-                subject=f"Vaše objednávka #{order.etb_id} je potvrzena [ZAPLACENO].",
-                from_email="objednavky@efirthebrand.cz",
-                to=[order.email],
-                bcc=["objednavky@efirthebrand.cz"],
-            )
-            msg.attach_alternative(html_content, "text/html")
-            msg.attach(f"Objednavka {order.etb_id}.pdf", out.getvalue(), "application/pdf")
+    msg = EmailMultiAlternatives(
+        subject=f"Vaše objednávka #{order.etb_id} je potvrzena [ZAPLACENO].",
+        from_email="objednavky@efirthebrand.cz",
+        to=[order.email],
+        bcc=["objednavky@efirthebrand.cz"],
+    )
+    msg.attach_alternative(html_content, "text/html")
 
-            msg.send()
-            logger.info(
-                "Customer order confirmation email sent successfully for order ID %s",
-                order_id,
-            )
+    if pdf_out:
+        msg.attach(f"Objednavka {order.etb_id}.pdf", pdf_out.getvalue(), "application/pdf")
 
-            order = Order.objects.get(id=order_id)
-            order.paid_confirmation_sent = True
-        
-            order.save()
+    try:
+        msg.send()
+        logger.info(
+            "Customer order confirmation email sent successfully for order ID %s",
+            order_id,
+        )
+        order.paid_confirmation_sent = True
+        order.save()
 
-            return True
+    except (ssl.SSLCertVerificationError, SSLError) as e:
+        logger.error("Failed to send email due to SSL error: %s", e)
 
-        except (
-            SMTPDataError,
-            SMTPException,
-            SMTPRecipientsRefused,
-            SMTPSenderRefused,
-        ) as e:
-            logger.error("Failed to send email: %s", e)
-            return False
+    except (
+        SMTPDataError,
+        SMTPException,
+        SMTPRecipientsRefused,
+        SMTPSenderRefused,
+    ) as e:
+        logger.error("Failed to send email: %s", e)
+        return False
 
-        except Order.DoesNotExist:
-            logger.error("Order with ID %s does not exist", order_id)
-            return False
+    except Exception as e:
+        logger.error("An unexpected error occurred: %s", e)
+        logger.info(
+            "Attempting to send customer order confirmation email for order ID without PDF %s",
+            order_id,
+        )
 
-        except Exception as e:
-            logger.error("An unexpected error occurred: %s", e)
-            logger.info(
-                "Attempting to send customer order confirmation email for order ID without PDF %s",
-                order_id,
-            )
-
-            order = Order.objects.get(id=order_id)
-
-            html_content = render_to_string(
-                "orders/customer_email_confirmation.html", {"order": order}
-            )
-            msg = EmailMultiAlternatives(
-                subject=f"Vaše objednávka #{order.etb_id} je potvrzena [ZAPLACENO].",
-                from_email="objednavky@efirthebrand.cz",
-                to=[order.email],
-                bcc=["objednavky@efirthebrand.cz"],
-            )
-            msg.attach_alternative(html_content, "text/html")
-
-            msg.send()
-            logger.info(
-                "Customer order confirmation email sent successfully wihtout PDF for order ID %s",
-                order_id,
-            )
-            order = Order.objects.get(id=order_id)
-            order.paid_confirmation_sent = True
-        
-            order.save()
-                
-            return False
+        return False
 
 
 from django.core.mail import EmailMultiAlternatives
