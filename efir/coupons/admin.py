6921676ff951c58f300e20d384074fc2c286638e
vlastimil.tetour <v.tetour@gmail.com>
2025-06-09 22:15:48 +0200

    adjusting coupon logic

--- efir/coupons/admin.py
+++ efir/coupons/admin.py
@@ -1,14 +1,27 @@
-from django.contrib import admin
-
-from .models import Coupon
-import xlwt
-
 import csv
 from datetime import datetime
 
-
+from django import forms
+from django.contrib import admin
 from django.http import HttpResponse
+from django.shortcuts import redirect, render
+from django.urls import path
 
+from .models import Coupon
+from .views import generate_voucher_code
+
+
+class GenerateCouponsForm(forms.ModelForm):
+    class Meta:
+        model = Coupon
+        exclude = ["code"]
+
+    def save(self, commit=True):
+        instance = super().save(commit=False)  # This calls ModelForm.save()
+        instance.code = generate_voucher_code(8)  # Auto-generate code
+        if commit:
+            instance.save()
+        return instance
 
 
 @admin.register(Coupon)
@@ -31,12 +44,50 @@ class CouponAdmin(admin.ModelAdmin):
     list_filter = ["active", "valid_from", "valid_to", "category"]
     search_fields = ["code"]
 
+    change_list_template = "admin/coupons_changelist.html"  # Extend this template
+
+    def get_urls(self):
+        urls = super().get_urls()
+        custom_urls = [
+            path(
+                "generate_new_coupons/",
+                self.admin_site.admin_view(self.generate_new_coupons),
+                name="generate_new_coupons",
+            ),
+        ]
+
+        return custom_urls + urls
+
+    def generate_new_coupons(self, request):
+        if request.method == "POST":
+            form = GenerateCouponsForm(request.POST)
+            if form.is_valid():
+                # Save instance, which sets code automatically
+                count = form.cleaned_data["capacity"]
+                coupons_created = 0
+                base_data = {
+                k: v for k, v in form.cleaned_data.items()
+                if k not in ["capacity", "code"]
+            }
+                for _ in range(count): 
+                    
+                    Coupon.objects.create(code=generate_voucher_code(8), **base_data)
+                    coupons_created += 1
+
+                self.message_user(request, "Kupón úspěšně vygenerován.")
+                return redirect("..")
+
+        else:
+            form = GenerateCouponsForm()
+        context = {"form": form}
+
+        return render(request, "admin/generate_coupons.html", context)
 
     @staticmethod
     def transfer_date(input_date):
         try:
-            date = datetime.strptime(str(input_date), '%Y-%m-%d')
-            transformed_date = date.strftime('%d.%m.%Y')  # Correct usage of strftime
+            date = datetime.strptime(str(input_date), "%Y-%m-%d")
+            transformed_date = date.strftime("%d.%m.%Y")  # Correct usage of strftime
             return transformed_date
         except ValueError:
             return "Invalid Format Date"
@@ -46,16 +97,20 @@ class CouponAdmin(admin.ModelAdmin):
         response["Content-Disposition"] = 'attachment; filename="coupons.csv"'
 
         writer = csv.writer(response)
-        writer.writerow([
-            "code",
-            "valid_to",  # Adjust header names as needed
-        ])
+        writer.writerow(
+            [
+                "code",
+                "valid_to",  # Adjust header names as needed
+            ]
+        )
 
         for obj in queryset:
-            writer.writerow([
-                obj.code,
-                self.transfer_date(obj.valid_to),  # Call transfer_date method
-            ])
+            writer.writerow(
+                [
+                    obj.code,
+                    self.transfer_date(obj.valid_to),  # Call transfer_date method
+                ]
+            )
 
         return response
 
--- efir/coupons/models.py
+++ efir/coupons/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 
 class Coupon(models.Model):
-    code = models.CharField(max_length=10, unique=True, help_text="Váš slevový kód:")
+    code = models.CharField(max_length=10, unique=True, help_text="Váš slevový kód:", blank=False)
     valid_from = models.DateField()
     valid_to = models.DateField()
-    capacity = models.IntegerField(default=1, verbose_name="Počet")
-    active = models.BooleanField(verbose_name="Aktivní")
-    redeemed = models.BooleanField(default=False, verbose_name="Uplatněno")
+    capacity = models.IntegerField(default=1, verbose_name="Počet", blank=False)
+    active = models.BooleanField(verbose_name="Aktivní", blank=False)
+    redeemed = models.BooleanField(default=False, verbose_name="Uplatněno", blank=False)
 
     DISCOUNT_TYPES = [
         ("Procento", "Procento"),
@@ -17,7 +17,7 @@ class Coupon(models.Model):
         max_length=255,
         choices=DISCOUNT_TYPES,
         verbose_name="Typ slevy",
-        blank=True,
+        blank=False,
     )
     discount_value = models.DecimalField(
         max_digits=10,
@@ -39,7 +39,7 @@ class Coupon(models.Model):
         max_length=10, default="N/A", verbose_name="Nakoupeno v objednavce (ID)"
     )
     certificate_from = models.CharField(
-        max_length=110, default="-", verbose_name="Do koho"
+        max_length=110, default="-", verbose_name="Od koho"
     )
     certificate_to = models.CharField(max_length=110, default="-", verbose_name="Komu")
 
@@ -47,12 +47,10 @@ class Coupon(models.Model):
         ("Dárkový certifikát", "Dárkový certifikát"),
         ("Sleva na první nákup", "Sleva na první nákup"),
         ("Odměna 30 dní po 1. nákupu", "Odměna 30 dní po 1. nákupu"),
-        (
-            "Sleva po 3 měsících", "Sleva po 3 měsících"
-        ),
+        ("Sleva po 3 měsících", "Sleva po 3 měsících"),
         ("Přání k svátku", "Přání k svátku"),
         ("Osobní slevový kód", "Osobní slevový kód"),
-("-", "-"),
+        ("-", "-"),
     ]
 
     category = models.CharField(
@@ -60,7 +58,7 @@ class Coupon(models.Model):
         choices=category_types,
         verbose_name="Kategorie",
         null=True,
-        blank=True,
+        blank=False,
     )
 
     def __str__(self):
