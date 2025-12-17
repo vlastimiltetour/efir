2c3f41798a9fe5e661258d95ff9f9781571e9609
vlastimil.tetour <v.tetour@gmail.com>
2025-12-17 08:28:44 +0100

    voucher zmena na procenta

--- efir/coupons/views.py
+++ efir/coupons/views.py
@@ -239,7 +239,7 @@ def generate_vouchers(request):
             orderitem_id=0,
             category="Sleva na první nákup",
             discount_value=10,
-            discount_type="Částka",
+            discount_type="Procento",
             discount_threshold=1,
             certificate_from="-",
             certificate_to="-",
